83cefda4e8294aa6e91fd83f521c0181cc869dbc
Hauptmannhuy <dindago2324@gmail.com>
2023-09-13 16:47:53 +0300

    implemented eventListener that adjusts size and rewrote pointerover eventLis

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 
 const container = document.getElementById('grid');
-function randomNum(){
-    let randomNum1 = Math.floor(Math.random(0)*256);
-    let randomNum2 = Math.floor(Math.random(0)*256);
-    let randomNum3 = Math.floor(Math.random(0)*256);
-    let array = [randomNum1,randomNum2,randomNum3];
-    return array;
-    
-}
-
-function randomRGB() {
-    const array = randomNum();
-    return  `rgb(${array[0]},${array[1]},${array[2]})`;
-   
-}
+const adjustBtn = document.getElementById('adjustBtn');
 
 
 function defaultGrid(rows,cols){
+    container.innerHTML = '';
     makeRows(rows,cols);
     makeCells(rows,cols);
     
@@ -38,15 +26,51 @@ function makeCells(rows,cols){
    
 }
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach((cell)=> {
-    cell.addEventListener('pointerover', (e) => {
+adjustBtn.addEventListener('click', (e) => {
+    let value = prompt('Enter a number');
+    defaultGrid(value,value);
+})
+
+
+
+function randomNum(){
+    let randomNum1 = Math.floor(Math.random(0)*256);
+    let randomNum2 = Math.floor(Math.random(0)*256);
+    let randomNum3 = Math.floor(Math.random(0)*256);
+    let array = [randomNum1,randomNum2,randomNum3];
+    return array;
+    
+}
+
+
+function randomRGB() {
+    const array = randomNum();
+    return  `rgb(${array[0]},${array[1]},${array[2]})`;
+   
+}
+
+container.addEventListener('pointerover', (e) => {
+    let target = e.target;
+    if (target.classList.contains('cell') === true){
         const rgb = randomRGB();
         function setRGB(rgb){
-            cell.style.setProperty('--background-color',`${rgb}`);
+            target.style.setProperty('--background-color',`${rgb}`);
        }
        setRGB(rgb)
-    });
-});
+    }
+})
+
+
+// const cells = document.querySelectorAll('.cell');
+// cells.forEach((cell)=> {
+//     cell.addEventListener('pointerover', (e) => {
+//         const rgb = randomRGB();
+//         function setRGB(rgb){
+//             cell.style.setProperty('--background-color',`${rgb}`);
+//        }
+//        setRGB(rgb)
+//     });
+// });
+
 
 
